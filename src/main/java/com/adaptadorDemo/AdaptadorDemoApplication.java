e90f95009a004cba73a65f30b0e7521b8ef24f35
José Flores <jose.osvaldo.flores@gmail.com>
2021-03-30 16:50:17 -0300

    test subida

--- src/main/java/com/adaptadorDemo/AdaptadorDemoApplication.java
+++ src/main/java/com/adaptadorDemo/AdaptadorDemoApplication.java
@@ -24,7 +24,6 @@ public class AdaptadorDemoApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		//adaptadorService = new AdaptadorServiceImp();
-
 		File archivo = new File("usuarios.xml");
 		adaptadorService.convertir(archivo);
 
